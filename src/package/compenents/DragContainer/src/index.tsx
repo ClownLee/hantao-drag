484e980ac5dbbbfe8b526b224a3f2e16e41a8767
ele_11_ven <lwentao@hotmail.com>
2023-02-13 00:34:00 +0800

    second

--- src/package/compenents/DragContainer/src/index.tsx
+++ src/package/compenents/DragContainer/src/index.tsx
@@ -19,13 +19,17 @@ export default defineComponent({
       const { dataset } = e.target;
       if(dataset && dataset.copy === 'true' ) {
         console.log('拖拽到容器')
+        e.dataTransfer.setData("Text", e.target.id);
       } else {
         console.log('容器内排序')
       }
     }
 
-    const dragLeave = (e: any) => {
-      console.log('目：leave', e)
+    const drop = (e: any) =>{
+      console.log('目标元素上松开鼠标', e)
+      var data = e.dataTransfer.getData("Text");
+      e.target.appendChild(document.getElementById(data));
+      console.log('哈哈哈')
     }
 
     return () => (
@@ -34,13 +38,15 @@ export default defineComponent({
         className='bg-red-500 bg-opacity-25 w-24 h-24 cursor-move'
         draggable='true'
         data-copy='true'
+        id='hahaha'
         onDragend={(e) => dragend(e)}>
           哈哈哈
         </div>
 
         <div
         className='w-full h-xs border-1 border-gray-500/20 border-dashed'
-        onDragleave={(e) => dragLeave(e)}>
+        onDragover={e => e.preventDefault()}
+        onDrop={(e) => drop(e)}>
 
         </div>
       </Div>
